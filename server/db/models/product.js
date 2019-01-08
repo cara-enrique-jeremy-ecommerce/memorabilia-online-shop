9b3bf0ec1e8b7eb5e390b73b10a977259599a6f7
gaeulko <gaeulko@gmail.com>
2019-01-08 13:36:02 -0800

    changes from comments

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -2,18 +2,24 @@ const db = require('../db')
 
 const Product = db.define('product', {
   name: Sequelize.STRING,
-  price: Sequelize.INTEGER,
-  description: Sequelize.STRING,
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    }
+  },
+  description: Sequelize.TEXT,
   image: {
     type: Sequelize.STRING,
-    defaultValue: 'defaultproduct.jpg'
+    defaultValue: '/defaultproduct.jpg'
   },
   quantity: Sequelize.INTEGER,
   stars: {
     type: Sequelize.INTEGER,
     validate: { min: 1, max: 5}
   },
-  reviews: Sequelize.STRING
+  //reviews: Sequelize.TEXT
 })
 
 module.exports = Product;
