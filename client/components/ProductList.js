2e66a9f8c212d45165c61e6198cb16e77bda8806
Jeremy Henrard <jhenrard@gmail.com>
2019-01-09 13:05:22 -0500

    feature: view all products by manually navigating to /products

---
+++ client/components/ProductList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import SingleProductSnapshot from './SingleProductSnapshot'
+import {fetchAllProducts} from '../store/products'
+
+class ProductList extends React.Component {
+  componentDidMount() {
+    this.props.fetchAllProducts()
+  }
+
+  render() {
+    const products = this.props.products
+
+    return (
+      <div>
+        {products &&
+          products.map(product => {
+            return <SingleProductSnapshot key={product.id} product={product} />
+          })}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAllProducts: () => dispatch(fetchAllProducts)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList)
