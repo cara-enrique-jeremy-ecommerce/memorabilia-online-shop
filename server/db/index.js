a2229d2914ec7cf916dbd35ca5516bb69fecc5ec
enrique novoa <luisenriquenovoa@gmail.com>
2019-01-08 20:58:13 -0800

    Category and Product association added.

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,10 @@
 const db = require('./db')
 
 // register models
-require('./models')
+const Category = require('./models/category')
+const Product = require('./models/product')
+
+Product.belongsTo(Category, {as: 'category'})
+Category.hasMany(Product)
 
 module.exports = db
