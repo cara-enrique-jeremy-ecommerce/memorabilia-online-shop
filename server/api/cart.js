223f2f11e2836c81a760dae2c8e48e44edf869b6
jhenrard <43557555+jhenrard@users.noreply.github.com>
2019-01-13 14:13:38 -0500

    passing off progress to enrique. routes do not work for cat (#61)

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,7 +1,35 @@
 const router = require('express').Router()
+const {Product, User} = require('../db/models')
 
 module.exports = router
 
-// router methods for user carts will go here.
-
 // guest carts are stored on state for now, but should probably use sessions or something to retain cart on page refresh.
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const productsInCart = await Product.findAll({
+      include: [
+        {
+          model: User,
+          through: {
+            where: {userId: req.params.userId}
+          }
+        }
+      ]
+    })
+    res.json(productsInCart)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// router.post('/cart/:userId', async (req, res, next) => {
+//   try {
+//     const addedProduct = await Product.create({userId: req.params.userId, productId: req.body.id}, {
+//       returning: true
+//     })
+//     res.json(addedProduct)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
