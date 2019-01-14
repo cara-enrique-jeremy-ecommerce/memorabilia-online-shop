e6cd718a08c28680f0de959127e89a0a0f787941
Cara Ko <gaeulko@gmail.com>
2019-01-14 12:59:03 -0800

    passing off (#70)

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {me} from '../store/user'
-import {fetchProductsInCart} from '../store/products'
+import {fetchProductsInCart, deleteProductInCart} from '../store/products'
 
 class Cart extends Component {
   constructor() {
@@ -14,7 +14,12 @@ class Cart extends Component {
     await this.props.getUser()
   }
 
+  async handleRemove(productId, userId) {
+    await this.props.deleteProductThunk(productId, userId)
+  }
+
   getCart = async userId => {
+    console.log('getting cart?')
     await this.props.getCartThunk(userId)
   }
   calcTotal = objects => {
@@ -25,51 +30,64 @@ class Cart extends Component {
     return `Total:   $${(total / 100).toFixed(2)}`
   }
   render() {
+    console.log('checking ', this.props)
     if (this.props.user.id && !this.state.gotCart) {
+      console.log('printing? ')
       this.getCart(this.props.user.id)
-      this.gotCart = true
+      this.setState({gotCart: true})
     }
-
+    console.log('cart length? ', this.props.cart, this.state.gotCart)
     return (
       <div>
-        <div className="flex center category-header">
+        <div>
           <h1>CART</h1>
         </div>
-        <ul className="cart-list">
-          {this.props.products.products ? (
-            this.props.products.products.map(product => {
-              return (
-                <li className="cart-list-item" key={product.id}>
-                  <div className="cart-item-inner">
-                    <img className="cart-item-img" src={product.photos[0]} />
+        {this.props.cart.length ? (
+          <div>
+            <ul>
+              {this.props.cart.map(product => {
+                return (
+                  <li key={product.id}>
                     <h2>{product.name}</h2>
-                  </div>
-                  <div className="cart-item-inner">
-                    <h2>3</h2>
-                    <div className="vr bgb h100" />
-                    <h2>{`$${(product.price / 100).toFixed(2)}`}</h2>
-                    <button className="remove-button hover-light">
-                      <h1>X</h1>
-                    </button>
-                  </div>
-                </li>
-              )
-            })
-          ) : (
-            <li>nothing in cart</li>
-          )}
-        </ul>
-        <div id="checkout-container">
-          <h2 id="total">
-            {this.props.products.products
-              ? this.calcTotal(this.props.products.products)
-              : 'Total:   $ 0.00'}
-          </h2>
-          <hr />
-          <button id="checkout-button">
-            <h2>Checkout</h2>
-          </button>
-        </div>
+                    {/* <Link to={`products/${product.id}`}>
+                        <img className="cart-item-img" src={product.image[0]} />
+                          <h2>{product.name}</h2>
+                      </Link> */}
+                    <div>
+                      {/* <h2>{product.orderItem.quantity}</h2> */}
+                      <div />
+                      <h2>{product.price}</h2>
+                      <button
+                        onClick={() =>
+                          this.handleRemove(product.id, this.props.user.id)
+                        }
+                      >
+                        <h1>X</h1>
+                      </button>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+            {this.props.products.length ? (
+              <div>
+                <h2>
+                  {this.props.products
+                    ? this.calcTotal(this.props.products)
+                    : 'Total:   $ 0.00'}
+                </h2>
+                <hr />
+                <button>
+                  <h2>Checkout</h2>
+                </button>
+              </div>
+            ) : (
+              <h2>empty cart, start shopping!</h2>
+            )}
+          </div>
+        ) : (
+          <h2>empty cart, start shopping!</h2>
+        )}
       </div>
     )
   }
@@ -77,12 +95,16 @@ class Cart extends Component {
 
 const mapStateToProps = state => ({
   user: state.user,
-  products: state.products
+  products: state.products,
+  cart: state.cart
 })
 
 const mapDispatchToProps = dispatch => ({
   getCartThunk: userId => {
     dispatch(fetchProductsInCart(userId))
+  },
+  deleteProductThunk: (productId, userId) => {
+    dispatch(deleteProductInCart(productId, userId))
   },
   getUser: () => {
     dispatch(me)
