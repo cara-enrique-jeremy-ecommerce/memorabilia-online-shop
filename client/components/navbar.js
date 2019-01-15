6dfd789da3f5c7e5c8634070423e4c0a215e8d55
enrique novoa <luisenriquenovoa@gmail.com>
2019-01-15 09:48:20 -0800

    Added the quantity in cart next to the icon

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -15,7 +15,7 @@ class Navbar extends React.Component {
   }
 
   render() {
-    const {handleClick, isLoggedIn, user, cart} = this.props
+    const {handleClick, isLoggedIn, cart} = this.props
     return (
       <div className="navbar">
         <Link to="/">
@@ -33,9 +33,9 @@ class Navbar extends React.Component {
               <Link to="/profile">
                 <i className="fa fa-user-circle-o" />
               </Link>
-              <Link to="/cart">
+              <Link className="shopping-cart" to="/cart">
                 <i className="fa fa-shopping-cart" />
-                {cart && <p>{cart.length}</p>}
+                {cart && <span>{cart.length}</span>}
               </Link>
             </div>
           ) : (
@@ -43,9 +43,9 @@ class Navbar extends React.Component {
               {/* The navbar will show these links before you log in */}
               <Link to="/login">Login</Link>
               <Link to="/signup">Sign Up</Link>
-              <Link to="/cart">
+              <Link className="shopping-cart" to="/cart">
                 <i className="fa fa-shopping-cart" />
-                <p>0</p>
+                <span>0</span>
               </Link>
             </div>
           )}
