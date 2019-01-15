8ba57634b0870cae1a8958fbbd67f6aa136d1ff7
jhenrard <43557555+jhenrard@users.noreply.github.com>
2019-01-14 23:24:42 -0500

    feat: fetch authenticated user carts from DB (#76)
    * feat: fetch user cart from DB (seeded data)
    
    * removed comment

--- server/api/cart.js
+++ server/api/cart.js
@@ -3,25 +3,23 @@ const {Product, User} = require('../db/models')
 
 module.exports = router
 
-// guest carts are stored on state for now, but should probably use sessions or something to retain cart on page refresh.
-
-router.get('/:userId', async (req, res, next) => {
-  try {
-    const productsInCart = await Product.findAll({
-      include: [
-        {
-          model: User,
-          through: {
-            where: {userId: req.params.userId}
-          }
-        }
-      ]
-    })
-    res.json(productsInCart)
-  } catch (error) {
-    next(error)
-  }
-})
+// router.get('/:userId', async (req, res, next) => {
+//   try {
+//     const productsInCart = await Product.findAll({
+//       include: [
+//         {
+//           model: User,
+//           through: {
+//             where: {userId: req.params.userId}
+//           }
+//         }
+//       ]
+//     })
+//     res.json(productsInCart)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 // router.post('/cart/:userId', async (req, res, next) => {
 //   try {
